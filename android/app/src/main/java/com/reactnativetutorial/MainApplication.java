9dd57e1e308bb53749ffbff3db8f2c75142572d1
Bryan Ching <bryan792@gmail.com>
2017-04-16 21:47:36 -0700

    Add realm

--- android/app/src/main/java/com/reactnativetutorial/MainApplication.java
+++ android/app/src/main/java/com/reactnativetutorial/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativetutorial;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.tradle.react.UdpSocketsModule;
 import com.peel.react.TcpSocketsModule;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new ReactVideoPackage(),
             new UdpSocketsModule(),
             new TcpSocketsModule(),
